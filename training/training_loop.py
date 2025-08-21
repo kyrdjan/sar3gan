b7772f28848166c3f95b9f0ebc3ab180cd39fead
kyrdjan <kyrdjan.marimon@carsu.edu.ph>
2025-08-21 12:13:57 +0800

    wfennf

--- training/training_loop.py
+++ training/training_loop.py
@@ -149,7 +149,7 @@ def training_loop(
     cudnn_benchmark         = True,     # Enable torch.backends.cudnn.benchmark?
     abort_fn                = None,     # Callback function for determining whether to abort training. Must return consistent results across ranks.
     progress_fn             = None,     # Callback function for updating training progress. Called for all ranks.
-    metric_eval_ticks       = 200,      # How often to evaluate metrics? None = disable.      
+    metric_eval_ticks       = 100,      # How often to evaluate metrics? None = disable.      
 ):
     # Initialize.
     start_time = time.time()
@@ -378,8 +378,7 @@ def training_loop(
                 )
 
             end_time = time.time()
-            print(f"[DEBUG] Loop over {len(label)} items took {end_time - start_time:.4f} seconds")
-
+            
             phase.module.requires_grad_(False)
 
             for g in phase.opt.param_groups:
